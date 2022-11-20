3328aa17ea3a310a4c5dccd20b061bade52e1959
SPRDNL73M24F224J <daniele.sparagna@gmail.com>
2022-11-20 21:12:23 +0100

    Modifica Daniele

--- scripts/mainODBC.py
+++ scripts/mainODBC.py
@@ -12,22 +12,17 @@ if not requested_driver in drivers:
     print(f"Missing requested driver {requested_driver} exit")
     exit(0)
 
-conn_str = (r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};'
-            r'DBQ=.\data\VIARIO.accdb;')
-conn = pyodbc.connect(conn_str)
+
 
 ld = listdir("data")
 
-# for f in ld:
-#     ex = os.path.splitext(f)[1]
-#     fullname = join("data", f)
-#     if isfile(fullname) :
-#         if ex == '.accdb]':
-#             print('ciao')
 
 access_files = [f for f in listdir("data") if os.path.isfile(join("data", f)) and os.path.splitext(f)[1] in ['.mdb', '.accdb']]
 
 for access_file in access_files:
+    conn_str = (r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};'
+                f'DBQ=.\\data\\{access_file};')
+    conn = pyodbc.connect(conn_str)
     sqlite_file = join("./data", os.path.splitext(access_file)[0]+'.sqlite')
 
     if os.path.isfile(sqlite_file):
@@ -39,7 +34,7 @@ for access_file in access_files:
     cursor = conn.cursor()
     for i in cursor.tables(tableType='TABLE'):
         print(i.table_name)
-        df = pd.read_sql(f'select * from {i.table_name}', conn)
+        df = pd.read_sql(f'select * from \"{i.table_name}\"', conn)
         df.to_sql(i.table_name, con=engine)
         print(f"table {i.table_name} of DB {access_file} imported into DB {sqlite_file}")
 
